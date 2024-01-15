ec5cda6ae95bc60e1b462fccc2664f7275bd3a41
Hanna Chen <hanna.yhchen@gmail.com>
2024-01-15 23:19:11 +0800

    Add info page

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
 import express from 'express'
 
+const persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456'
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523'
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345'
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122'
+  }
+]
+
 const PORT = 3001
 const app = express()
 app.use(express.json())
 
 app.get('/api/persons', (request, response) => {
-  const data = [
-    {
-      id: 1,
-      name: 'Arto Hellas',
-      number: '040-123456'
-    },
-    {
-      id: 2,
-      name: 'Ada Lovelace',
-      number: '39-44-5323523'
-    },
-    {
-      id: 3,
-      name: 'Dan Abramov',
-      number: '12-43-234345'
-    },
-    {
-      id: 4,
-      name: 'Mary Poppendieck',
-      number: '39-23-6423122'
-    }
-  ]
-  response.json(data)
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  const date = new Date()
+  response.send(/* html */ `
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${(date).toString()}</p>
+  `)
 })
 
 app.listen(PORT, () => {
